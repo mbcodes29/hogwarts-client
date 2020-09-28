7da47fa5d047456b4a27b9b708f70685824fe9ed
Madison Brown <59658823+mbcodes29@users.noreply.github.com>
2020-09-28 01:45:58 -0400

    test

--- src/routes/CreatePage/CreatePage.js
+++ src/routes/CreatePage/CreatePage.js
@@ -1,20 +1,20 @@
-import React from 'react'
-import './CreatePage.css'
-import Header from '../../components/Header/Header'
-import CreatePageForm from '../../components/CreatePageForm/CreatePageForm'
-import NavBar from '../../components/Nav/NavBar'
+import React from "react"
+import "./CreatePage.css"
+import Header from "../../components/Header/Header"
+import CreatePageForm from "../../components/CreatePageForm/CreatePageForm"
+import NavBar from "../../components/Nav/NavBar"
 
 export default class CreatePage extends React.Component {
   goTo = () => {
-    this.props.history.push('/my-students')
+    this.props.history.push("/my-students")
   }
   render() {
     return (
       <div>
         <Header />
         <NavBar />
-        <CreatePageForm goTo={this.goTo}/>
+        <CreatePageForm goTo={this.goTo} />
       </div>
-    );
-  } 
+    )
+  }
 }
